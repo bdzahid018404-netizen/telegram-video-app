4a8cf0baefb4dbd3b31fff9763e2c1bb36603b79
bdzahid018404-netizen <bdzahid018404@gmail.com>
2026-09-22 15:22:38 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, send_from_directory
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+WEB_APP_URL = os.environ.get("WEB_APP_URL")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = InlineKeyboardMarkup()
+    web_app_button = InlineKeyboardButton(
+        text="🎬 Open Video App", 
+        web_app=WebAppInfo(url=WEB_APP_URL)
+    )
+    markup.add(web_app_button)
+    bot.reply_to(message, "স্বাগতম! ভিডিও দেখতে নিচের বাটনে চাপ দিন:", reply_markup=markup)
+
+if __name__ == '__main__':
+    from threading import Thread
+    def run_bot():
+        bot.infinity_polling()
+    
+    Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+  
